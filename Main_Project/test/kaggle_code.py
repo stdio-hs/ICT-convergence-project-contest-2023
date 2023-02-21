1b87a790a85fbe5ce203e143d4eb79eb33229f0e
김민관 <alsrhks2508@naver.com>
2023-02-21 20:37:24 +0900

    인도인의 keras 학습 모델

--- Main_Project/test/kaggle_code.py
+++ Main_Project/test/kaggle_code.py
@@ -7,48 +7,115 @@ from skimage import io
 import os
 import random
 import matplotlib.pyplot as plt
-# matplotlib inline
-
 import glob
 
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization
+from keras.layers import Activation, Dropout
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from keras.optimizers import Adam
 
-def segment(image, threshold=25):
-    global bg
-    # find the absolute difference between background and current frame
-    diff = cv2.absdiff(bg.astype("uint8"), image)
 
-    # threshold the diff image so that we get the foreground
-    thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
+# model
+model = Sequential()
 
-    # get the contours in the thresholded image
-    (cnts, _) = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# first conv layer
+# input shape = (img_rows, img_cols, 1)
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(100,120, 1))) 
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-    # return None, if no contours detected
-    if len(cnts) == 0:
-        return
-    else:
-        # based on contour area, get the maximum contour which is the hand
-        segmented = max(cnts, key=cv2.contourArea)
-        return (thresholded, segmented)
+# second conv layer
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+# flatten and put a fully connected layer
+model.add(Flatten())
+model.add(Dense(128, activation='relu')) # fully connected
+model.add(Dropout(0.5))
+
+# softmax layer
+model.add(Dense(6, activation='softmax'))
+
+# model summary
+optimiser = Adam() 
+model.compile(optimizer=optimiser, loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.summary()
+
+DATASET_PATH = '/kaggle/input/hand-gesture-recog-dataset/data'
+
+dataset_path = os.path.join(DATASET_PATH, '*')
+import glob
+dataset_path = glob.glob(dataset_path)
+dataset_path
+
+import cv2
+image = cv2.imread('/kaggle/input/hand-gesture-recog-dataset/data/five/hand1(1015).jpg')
+image = cv2.resize(image,(100, 120))
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+loaded_images = []
+
+list_of_gestures = ['blank', 'ok', 'thumbsup', 'thumbsdown', 'fist', 'five']
+
+for path in range(0, len(dataset_path)):
+    dataset_path = "/kaggle/input/hand-gesture-recog-dataset/data/" + str(list_of_gestures[path])
+    gesture_path = os.path.join(dataset_path, '*')
+    import glob
+    gest_path = glob.glob(gesture_path)
+    k = 0
+    for i in range(0, len(gest_path)):
+        if k < 1600:
+            image = cv2.imread(gest_path[i])
+            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            gray_image = cv2.resize(gray_image,(100, 120))
+            loaded_images.append(gray_image)
+        k=k+1
+print(len(loaded_images))
+
+outputVectors = []
+for i in range(1, 1601):
+    outputVectors.append([1, 0, 0, 0, 0, 0])
+
+for i in range(1, 1601):
+    outputVectors.append([0, 1, 0, 0, 0, 0])
+
+for i in range(1, 1601):
+    outputVectors.append([0, 0, 1, 0, 0, 0])
     
+for i in range(1, 1601):
+    outputVectors.append([0, 0, 0, 1, 0, 0])
+    
+for i in range(1, 1601):
+    outputVectors.append([0, 0, 0, 0, 1, 0])
 
-# Starts capturing video
-cap = cv2.VideoCapture(0)
+for i in range(1, 1601):
+    outputVectors.append([0, 0, 0, 0, 0, 1])
 
-fps = int(cap.get(cv2.CAP_PROP_FPS))
+print(len(outputVectors))
 
-print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+X = np.asarray(loaded_images)
+y = np.asarray(outputVectors)
+print(X.shape)
+print(y.shape)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    th_f = segment(frame)
-    cv2.imshow('Captured Frame', th_f)
-    if cv2.waitKey(1) == ord('q'):
-        break
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
+X_train = X_train.reshape(X_train.shape[0], 100, 120, 1)
+X_test = X_test.reshape(X_test.shape[0], 100, 120, 1)
+print(X_train.shape)
+print(X_test.shape)
 
-    keypress = cv2.waitKey(1) & 0xFF
+model.fit(X_train, y_train,
+          batch_size=128,
+          epochs=10,
+          verbose=1,
+          validation_data=(X_test, y_test))
 
-
-
-cap.release()
-cv2.destroyAllWindows()
+model.save("hand_gesture_recognition.h5")
