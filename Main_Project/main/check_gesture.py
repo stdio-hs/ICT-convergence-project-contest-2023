f444fa8ac63fe215bf407e63867b339e0ca747ed
김민관 <alsrhks2508@naver.com>
2023-02-24 20:37:13 +0900

    모델의 변경 및 클래스화

--- Main_Project/main/check_gesture.py
+++ Main_Project/main/check_gesture.py
@@ -1,25 +1,27 @@
 import cv2
-import numpy as np
 import os
 
 import matplotlib.pyplot as plt
 import glob
 import common2 as cm
 
-model_path = ('./../test/')
-model_name = 'model.pt'
+
+
+model_path = ('./../../../finger_model1/')
 
 
 def main():
     cap = cv2.VideoCapture(0)
+    k = 0
+    accumWeight = 0.1
+    num_frames = 0
 
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
-
-    num_frames = 0
-
+    
+    model = cm.model_load(model_path)
     #model = cm.model_load(model_path, model_name)
-
+    
     while cap.isOpened():
         ret, frame = cap.read()
 
@@ -28,16 +30,39 @@ def main():
             print(" >> Camera did not work, check your port or status << ")
             break
 
-        cv_frame = cm.VideoCustom(frame)
-    
+        cv_frame = cm.VideoCustom(frame, fps)
+
+
+        #처음 30프레임은 배경을 위해 버림
+        if num_frames < 30:
+            cv_frame.run_avg(accumWeight)
+            #cm.run_avg(cv_frame.get_gray(), accumWeight)
+            if num_frames == 1:
+                print("[STATUS] please wait! calibrating...")
+            elif num_frames == 29:
+                print("[STATUS] calibration successfull...")
+        
+        else:
+            hand = cv_frame.segment()
+            #hand = cm.segment(cv_frame.get_gray())
+            # 여기 이프 부분 common2로 이동시키고, 클래스에서 통합관리 시키기
+            if hand is not None:
+                cm.roi_workin(hand, cv_frame, model, k)
+
+        k += 1
+        #손이 들어가는 네모 그리기
+        top, right, bottom, left = cv_frame.getSize()
+        cv2.rectangle(cv_frame.clone, (left, top), (right, bottom), (0, 255, 0), 2)
+        #프레임 수 체크
+        num_frames += 1
+
         #화면에 송출
-        cv2.imshow('Captured Frame : ', cv_frame.get_frame())
+        cv2.imshow("Video Feed", cv_frame.clone)
 
-
-        # q를 누르면 종료 
-        if cv2.waitKey(1) == ord('q'):
-            break
         keypress = cv2.waitKey(1) & 0xFF
+
+        if keypress == ord("q"):
+            break
 
     #cv2 프레임 메모리 반환
     cap.release()
--- Main_Project/main/common2.py
+++ Main_Project/main/common2.py
@@ -1,49 +1,168 @@
 import pickle
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import img_to_array
+from sklearn import preprocessing
 #from typing import Self
+labels = ['blank', 'fist', 'five', 'ok', 'thumbsdown', 'thumbsup']
+f_label = ['0L', '0R', '1L','1R' , '2L', '2R', '3L', '3R', '4L', '4R', '5L', '5R',]
 
-def model_load(model_path, model_name):
-    model = pickle.load(model_path + model_name)
+def model_load(model_path):
+    print('model_loading')
+    #model = pickle.load(model_path + model_name)
+    model = load_model(model_path)
+    print(model.summary())
     return model
+
+def _load_weights():
+    try:
+        model = model_load("hand_gesture_recog_model.h5")
+        print(model.summary())
+        # print(model.get_weights())
+        # print(model.optimizer)
+        return model
+    except Exception as e:
+        return None
+    
+def getPredict(model, frame):
+    prediction = _predict(model, frame)
+    if prediction is not None:
+        #prediction = np.array(prediction)
+        classFound = np.array(prediction)
+        #classFound = np.asarray(prediction, dtype = int)
+        label = _getLabel(classFound)
+        return label 
+
+    return prediction
+
+def _predict(model, frame):
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    cv2.resize(gray_image, (100, 120))
+    gray_image = img_to_array(gray_image)
+    gray_image = np.expand_dims(gray_image, axis = 0)
+
+    prediction = model.predict(gray_image)
+    return prediction
+
+def _getLabel(predict):
+    print(predict)
+    arg_class = np.argmax(predict)
+    print(arg_class)
+    predict_label = f_label[arg_class]
+    return predict_label
+
+
 
 
 #-------------------------------------------------------------
 import cv2
 
+#글로벌 변수 (배경)
+bg = None
+
+
+#cv2로 받은 비디오를 총괄하여 관리하는 클래스
+#240 215
 class VideoCustom():
-    top, right, bottom, left = 10, 350, 225, 590
+    #top, right, bottom, left = 30, 200, 130, 320
+    #top, right, bottom, left = 29, 200, 270, 415
+    # finger set 
+    top, right, bottom, left = 30, 200, 158, 328
+    #gray, frame, roi
 
-    def __init__(self, frame):
+    def __init__(self, frame, fps):
         self.customFrame(frame)
-        self.cvGray(self.roi)
-        
+        self.cvGray()
+        self.fps = fps
 
+    def getSize(self):
+        return self.top, self.right, self.bottom, self.left
+        
     def customFrame(self, frame):
         #프레임 크기 변형
-        self.frame = cv2.resize(frame, (700, 700))
+        self.frame = frame
+        #self.frame = cv2.resize(frame, (700, 700))
         #프레임 좌우 반전
-        self.frame = cv2.flip(frame, 1)
-
-        self.frame = frame.copy()
-        self.roi = self.make_roi(frame)
+        #self.frame = cv2.flip(frame, 1)
+        self.clone = self.frame.copy()
+        (self.height, self.width) = frame.shape[:2]
+        self.make_roi(frame)
 
     #convert Frame as grayFrame
-    def cvGray(self, roi):
-        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        self.gray = cv2. GaussianBlur(gray, (7, 7), 0)
+    def cvGray(self):
+        gray = cv2.cvtColor(self.roi, cv2.COLOR_BGR2GRAY)
+        self.gray = cv2.GaussianBlur(gray, (0, 0), 1 )
+        #self.gray = cv2.normalize(gray, None, 0, 255, cv2.NORM_MINMAX)
+        
 
     ##region of interest
     def make_roi(self, frame):
-        roi = frame[self.top:self.bottom, self.right:self.left]
-        return roi
+        self.roi = frame[self.top:self.bottom, self.right:self.left]
+
+    #배경 찍어내기
+    def run_avg(self, accumWeight):
+        global bg
+        # initialize the background
+        if bg is None:
+            bg = self.gray.copy().astype("float")
+            return
+
+        # compute weighted average, accumulate it and update the background
+        cv2.accumulateWeighted(self.gray, bg, accumWeight)
+        return 
+
+    #배경으로 부터 손부분 잘라내기
+    def segment(self, threshold=15):
+        global bg
+        # find the absolute difference between background and current frame
+        #print(self.bg)
+        if bg is not None:
+            cv2.imshow("gray", self.gray)
+            diff = cv2.absdiff(bg.astype("uint8"), self.gray)
+            temp = np.array(diff)
+            #print(temp.mean())
+
+            # 차이가 threshold 보다 크면 흰색, threshold보다 작으면 검은색
+            thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
+
+            # get the contours in the thresholded image
+            (cnts, _) = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            # return None, if no contours detected
+            if len(cnts) == 0:
+                return
+            else:
+                # based on contour area, get the maximum contour which is the hand
+                segmented = max(cnts, key=cv2.contourArea)
+                return (thresholded, segmented)
+        
+        return
     
-    def get_frame(self):
-        return self.frame
+
+def roi_workin(hand, cv_frame, model, k):
+    (thresholded, segmented) = hand
+
+    #roi 내부의 외각선 그리기
+    cv2.drawContours(cv_frame.clone, [segmented + (cv_frame.right, cv_frame.top)], -1, (0, 0, 255))
+    #print('k = ', k)
+    #print('fps = ', cv_frame.fps)
     
-    def get_roi(self):
-        return self.roi
+
+    if k % (cv_frame.fps // 6) == 0:
+        #스레드홀드 temp.png로 보관
+        cv2.imwrite('Temp.png', thresholded)
+        #예측
+        predictedClass = getPredict(model, cv_frame.gray)
+        print(predictedClass)
+        cv2.putText(cv_frame.clone, str(predictedClass), (70, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    # 스레드 홀드 보여주기
+    cv2.imshow("Thesholded", thresholded)
 
 
-    
+
+
 
 
         
