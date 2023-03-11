9c1a96f8d1c69e9dafa41c7a306088757d87d9f2
김민관 <alsrhks2508@naver.com>
2023-03-11 20:23:11 +0900

    Update utils.py

--- Main_Project/posenetTest/remake_posenet/posenet/utils.py
+++ Main_Project/posenetTest/remake_posenet/posenet/utils.py
@@ -39,7 +39,10 @@ def read_cap(cap, scale_factor = 1.0, output_stride = 16):
     #print(input_img * (2.0 / 255.0) - 1.0)
 
     input_img = input_img * (2.0 / 255.0) - 1.0
-    #cv2.imshow('utils', input_img)
+    #cv2.imshow('utils', input_img) #아주 어둡게 출력되는 것을 알 수 있다. 왜?
+    #실험 결과 곱하는 값이 크면 클수록 밝에 나오는데, 2.0 대신 10을 넣으면 아주 약간만 인식된다.
+    #반대로 어두운 것은 상관없이 얼마나 어둡든 잘 찍혀나온다는 것을 알 수 있었다.
+
     # model의 차원에 맞추어 4차원 형태로 변형
     input_img = input_img.reshape(1, target_height, target_width, 3)
 
