b57d4c5626120ba08bb7afaac3706f644524fc6a
김민관 <alsrhks2508@naver.com>
2023-03-11 17:56:29 +0900

    tcp 통신부분 삭제

--- Main_Project/posenetTest/posenet-python-master/webcam_demo.py
+++ Main_Project/posenetTest/posenet-python-master/webcam_demo.py
@@ -16,21 +16,12 @@ parser.add_argument('--model', type=int, default=101)
 parser.add_argument('--cam_id', type=int, default=0)
 parser.add_argument('--cam_width', type=int, default=1280)
 parser.add_argument('--cam_height', type=int, default=720)
-parser.add_argument('--scale_factor', type=float, default=0.7125)
+parser.add_argument('--scale_factor', type=float, default=0.8) #0.7125
 parser.add_argument('--file', type=str, default=None, help="Optionally use a video file instead of a live camera")
 args = parser.parse_args()
 
-def recvall(sock, count):
-    # 바이트 문자열
-    buf = b''
-    while count:
-        newbuf = sock.recv(count)
-        if not newbuf: return None
-        buf += newbuf
-        count -= len(newbuf)
-    return buf
 
-def posenetTest():
+def main():
     with tf.Session() as sess:
         model_cfg, model_outputs = posenet.load_model(args.model, sess)
         output_stride = model_cfg['output_stride']
@@ -72,40 +63,6 @@ def posenetTest():
                 break
 
         print('Average FPS: ', frame_count / (time.time() - start))
-
-def main():
-    #socket에서 수신한 버퍼를 반환하는 함수
-    
-    #HOST = socket.gethostbyname('bd4a-125-185-34-18.jp.ngrok.io') #이건 http라서 안됨 ㅇㅇ
-    #HOST='192.168.0.13'
-    #HOST= '127.0.0.1"
-    PORT=5335
-    
-    #TCP 사용
-    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    print('Socket created')
-    
-    #서버의 아이피와 포트번호 지정
-    #s.bind((HOST,PORT))
-    s.bind(( socket.gethostname(),PORT))
-    print('Socket bind complete')
-    # 클라이언트의 접속을 기다린다. (클라이언트 연결을 1개까지 받는다)
-    s.listen(1)
-    print('Socket now listening')
-    
-    #연결, conn에는 소켓 객체, addr은 소켓에 바인드 된 주소
-    conn,addr=s.accept()
-    
-    while True:
-        # client에서 받은 stringData의 크기 (==(str(len(stringData))).encode().ljust(16))
-        length = recvall(conn, 16)
-        stringData = recvall(conn, int(length))
-        data = np.fromstring(stringData, dtype = 'uint8')
-        
-        #data를 디코딩한다.
-        frame = cv2.imdecode(data, cv2.IMREAD_COLOR)
-        posenetTest(frame)
-        cv2.waitKey(1)
 
 if __name__ == "__main__":
     main()
