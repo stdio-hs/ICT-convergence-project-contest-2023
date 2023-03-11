4a1733161846825167faf6d62d32fec969560a25
김민관 <alsrhks2508@naver.com>
2023-03-11 17:56:14 +0900

    유틸리티

--- Main_Project/posenetTest/posenet-python-master/posenet/utils.py
+++ Main_Project/posenetTest/posenet-python-master/posenet/utils.py
@@ -7,18 +7,27 @@ import posenet.constants
 def valid_resolution(width, height, output_stride=16):
     target_width = (int(width) // output_stride) * output_stride + 1
     target_height = (int(height) // output_stride) * output_stride + 1
+
     return target_width, target_height
 
 
 def _process_input(source_img, scale_factor=1.0, output_stride=16):
+
+
     target_width, target_height = valid_resolution(
         source_img.shape[1] * scale_factor, source_img.shape[0] * scale_factor, output_stride=output_stride)
     scale = np.array([source_img.shape[0] / target_height, source_img.shape[1] / target_width])
 
     input_img = cv2.resize(source_img, (target_width, target_height), interpolation=cv2.INTER_LINEAR)
     input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB).astype(np.float32)
-    input_img = input_img * (2.0 / 255.0) - 1.0
+    #0.00784
+    #input_img = input_img * (2.0 / 255.0) - 1.0
+    input_img = input_img * (10.0 / 255.0) - 1.0
+    
+    cv2.imshow('utils', input_img)
     input_img = input_img.reshape(1, target_height, target_width, 3)
+    print(input_img)
+
     return input_img, source_img, scale
 
 
@@ -26,6 +35,7 @@ def read_cap(cap, scale_factor=1.0, output_stride=16):
     res, img = cap.read()
     if not res:
         raise IOError("webcam failure")
+
     return _process_input(img, scale_factor, output_stride)
 
 
